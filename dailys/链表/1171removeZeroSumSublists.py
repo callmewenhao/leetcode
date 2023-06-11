29d8b0d6e6f41759ebe96df3e41d9954572c6bff
zhaowenhao <54170620+callmewenhao@users.noreply.github.com>
2023-06-11 19:48:29 +0800

    update

--- dailys/链表/1171removeZeroSumSublists.py
+++ dailys/链表/1171removeZeroSumSublists.py
@@ -3,29 +3,61 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from typing import Optional
+from typing import Optional, Tuple
 from List import ListNode
 from collections import defaultdict
 
 
 class Solution:
+
+    # 前缀和 + hash优化
+    # 第一次遍历确定 last 位置
+    # 第二次遍历确定 节点指向
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        dict = defaultdict(int)
+        dict = defaultdict(ListNode)
+
+        s = 0
+        cur = dummy
+        while cur:
+            s += cur.val
+            dict[s] = cur
+            cur = cur.next
+
+        s = 0
+        cur = dummy
+        while cur:
+            s += cur.val
+            if s in dict:
+                cur.next = dict[s].next
+            cur = cur.next
+
+        return dummy.next
+
+    # 朴素做法 On^2
+
+    def removeZeroSumSublists1(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
 
         sum = 0
         cur = dummy
         st = []
+
+        def find(s: int) -> Tuple[int, ListNode]:
+            for i, (val, node) in enumerate(st):
+                if val == s:
+                    return i, node
+            return -1, None
+
         while cur:
             sum += cur.val
-            
-            if sum in dict:
-                st[dict[sum]].next = cur.next
-                st = st[:dict[sum]+1]
-                dict.pop(sum)
+
+            idx, node = find(sum)
+            if idx != -1:
+                node.next = cur.next
+                st = st[:idx+1]
             else:
-                dict[sum] = len(st)
-                st.append(cur)
-            
+                st.append((sum, cur))
+
             cur = cur.next
         return dummy.next
